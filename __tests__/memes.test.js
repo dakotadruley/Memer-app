100608afc06a638560d1a36ed1d664bdd6c3c9bc
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-04-05 15:32:22 -0700

    added my first test now to write my route

--- __tests__/memes.test.js
+++ __tests__/memes.test.js
@@ -1,9 +1,28 @@
 const { getMeme, getMemes } = require('../db/data-helpers');
+const request = require('supertest');
+const app = require('../lib/app');
 
-
-
-
-
+describe('meme routes', () => {
+  it('creates a meme', async() => {
+    
+    return request(app) 
+      .post('api/v1/memes')
+      .send({
+        top: 'test top',
+        image: 'testingimage@thistest.com',
+        bottom: 'test bottom'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          top: 'test top',
+          image: 'testingimage@thistest.com',
+          bottom: 'test bottom',
+          __v: 0
+        });
+      });
+  });
+});
 // POST /api/v1/memes to create a meme
 // GET /api/v1/memes to get all memes
 // GET /api/v1/memes/:id to get a meme by id
