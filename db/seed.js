f51b5c0f4c7c6f6039ff75b51d7c8855e83853de
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-04-05 15:19:38 -0700

    updated seed data

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,15 @@
 const Meme = require('../lib/models/Meme');
+const chance = require('chance').chance();
+
+module.exports = async({ memesToCreate = 10 }) => {
+  
+  const tops = ['When you had too much', 'Stuck inside all day be like', 'When you procrastinate like', 'Looks like I should clean', 'Going for a walk seems nice'];
+  const images = ['https://media.giphy.com/media/cfuL5gqFDreXxkWQ4o/giphy.gif', 'https://media.giphy.com/media/6VoDJzfRjJNbG/giphy.gif', 'https://media.giphy.com/media/1ViLp0GBYhTcA/giphy.gif', 'https://media.giphy.com/media/kv71PxaD9P3R6/giphy.gif', 'https://media.giphy.com/media/eAk2gsB40pZJK/giphy.gif'];
+  const bottoms = ['But there is always more', 'YEEE HAHHH!', 'Don\'t play with me', 'What can I say?', 'Haters gunna hate'];
+
+  await Meme.create([...Array(memesToCreate)].map(() => ({
+    top: chance.pickone(tops),
+    image: chance.pickone(images),
+    bottom: chance.pickone(bottoms)
+  })));
+};
