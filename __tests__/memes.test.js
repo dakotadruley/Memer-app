78ca04c3717feee9e04cfd760087083acb38664b
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-04-05 16:01:40 -0700

    get by id route and test passing

--- __tests__/memes.test.js
+++ __tests__/memes.test.js
@@ -32,9 +32,18 @@ describe('meme routes', () => {
         expect(res.body).toEqual(memes);
       });
   });
+
+  it('gets a meme by id', async() => {
+    const meme = await getMeme();
+
+    return request(app) 
+      .get(`/api/v1/memes/${meme._id}`)
+      .then(res => {
+        expect(res.body).toEqual(meme);
+      });
+  });
 });
-// POST /api/v1/memes to create a meme
-// GET /api/v1/memes to get all memes
+
 // GET /api/v1/memes/:id to get a meme by id
 // PUT /api/v1/memes/:id to update a meme
 // DELETE /api/v1/memes/:id to delete a meme
